321849a13f1ca55be1fcc088bc21f5e974d98f0e
川崎智絵 <tme924@gmail.com>
2016-06-20 21:41:20 +0900

    最初のコミット

---
+++ Assets/Script/GameStartScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameStartScript : MonoBehaviour {
+		
+    private GameObject main;
+
+		void Start (){
+			main = GameObject.Find ("Title");
+		}
+
+		void Update (){
+			for (int i = 0; i < Input.touchCount; i++) {
+				Touch touch = Input.GetTouch (i);
+			if (touch.phase == TouchPhase.Began){
+				SceneManager.LoadScene ("Selection");
+				}
+			}
+			if (Input.GetMouseButtonDown (0)) {
+				SceneManager.LoadScene("Selection");
+			}
+		}
+}		
