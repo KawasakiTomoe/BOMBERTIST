15b122c6bb397abeb91d6883392c5b016f2df120
川崎智絵 <tme924@gmail.com>
2016-06-22 10:35:54 +0900

    ボタンの追加・実験資料の追加

---
+++ Assets/Script/ButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour {
+
+	public void OnClick (int number){
+
+		switch (number) {
+		case 0:
+			SceneManager.LoadScene ("Selection1");
+			break;
+		case 1:
+			SceneManager.LoadScene ("Selection2");
+			break;
+		case 2:
+			SceneManager.LoadScene ("Selection3");
+			break;
+		case 3:
+			SceneManager.LoadScene ("Selection4");
+			break;
+		}
+	}
+}
